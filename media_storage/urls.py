d74b47cd61ed04ce9fb4993982534271bb17b7f4
iShadow <ilya.dinaburgskiy@gmail.com>
2022-11-17 23:32:35 +0300

    Create Models+Admins+DRF+Channels+Search+Celery+Notifications with WebSocket+Verification phones

--- media_storage/urls.py
+++ media_storage/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import MediaViewSet, MediaRatingViewSet, MediaChatJoinAPIView, MediaSearchAPIView, MediaOrderAPIView
+from .views import MediaViewSet, MediaRatingViewSet, MediaChatJoinAPIView, MediaSearchAPIView, MediaOrderAPIView, \
+    MediaByAuthorAPIView
 
 router = SimpleRouter()
 router.register('media', MediaViewSet, basename='media')
@@ -10,6 +11,7 @@ router.register('rating', MediaRatingViewSet, basename='media rating')
 urlpatterns = router.urls
 urlpatterns.append(path('media/join/<int:pk>/', MediaChatJoinAPIView.as_view()),)
 urlpatterns.append(path('media/search', MediaSearchAPIView.as_view(), name='media search'),)
+urlpatterns.append(path('media/author', MediaByAuthorAPIView.as_view(), name='media by author'),)
 urlpatterns.append(path('media/order', MediaOrderAPIView.as_view(), name='media order'),)
 
 urlpatterns = format_suffix_patterns(urlpatterns)
